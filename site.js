4e9eeb1a303dff9e5377d39684ac9d2220e06504
James <300089902@fvtc.edu>
2024-03-01 12:35:54 -0600

    Added secret message

--- site.js
+++ site.js
@@ -10,11 +10,13 @@ const isEvening = hours >= 17 || hours < 4;
 const goodDay = document.querySelector('#welcome');
 
 if (isMorning) {
-    goodDay.textContent = `Good Morning! Thank you for visiting!`;
+    goodDay.textContent = 'Good Morning! Thank you for visiting!';
 }
 else if (isAfternoon) {
-    goodDay.textContent = `Good Afternoon! Thank you for visiting!`;
+    goodDay.textContent = 'Good Afternoon! Thank you for visiting!';
 }
 else {
-    goodDay.textContent = `Good Evening! Thank you for visiting!`;
+    goodDay.textContent = 'Good Evening! Thank you for visiting!';
 }
+
+localStorage.setItem("It's a secret to everybody.", 'You got Marin! Is this your big chance?');
