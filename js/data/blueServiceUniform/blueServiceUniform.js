70f294dc17164d5e9e2de211d1a14adff8331299
James <300089902@fvtc.edu>
2025-08-28 11:49:43 -0500

    Added princess options for Class Bs

--- js/data/blueServiceUniform/blueServiceUniform.js
+++ js/data/blueServiceUniform/blueServiceUniform.js
@@ -4,16 +4,16 @@ export default [
         wearer: "Male", group: "Male Trousers", value: "male_trousers", label: "Male Trousers", level: null, image: `/js/data/blueServiceUniform/images/Trousers.svg`, reference: "4.1.11.1.1.4. Trousers. Trousers will be blue and trim-fitted with no bunching at the waist or bagging at the seat.The trousers will be full cut, straight hanging and without cuffs or additional alterations to further taper the leg.The front of the trouser legs will rest on the front of the shoes or boots with a slight break in the crease.The back of the trouser legs will be ⅞ inch longer than the front.The trouser material will match the service dress coat in both fabric and shade 4.1.11.1.1.5. Belt and Buckle.The belt will be blue, cotton web or elastic, solid or woven.The belt will have a plain silver(chrome finish) tip and a buckle, except for General Officers who will wear the Wing and Star on the buckle. The silver tip of the belt will extend beyond the buckle facing the wearer’s left; however, no blue fabric will be visible between the silver tip and silver buckle. 4.1.11.1.1.6. Footwear. Black socks and Low Quarters, Dress Boots, or Combat Boots are required with this uniform.",
     },
     {
-        wearer: "Male", group: "Male Shirt", value: "short_sleeve_no_tie", label: "Short Sleeve Shirt - No Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_S.svg`, reference: null,
+        wearer: "Male", group: "Male Shirt", value: "short_sleeve", label: "Short Sleeve Shirt", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_S.svg`, reference: null,
     },
     {
-        wearer: "Male", group: "Male Shirt", value: "short_sleeve_with_tie", label: "Short Sleeve Shirt - With Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_S_Tie.svg`, reference: null,
+        wearer: "Male", group: "Male Shirt", value: "short_sleeve_tie", label: "Short Sleeve Shirt - With Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_S_Tie.svg`, reference: null,
     },
     {
-        wearer: "Male", group: "Male Shirt", value: "long_sleeve_no_tie", label: "Long Sleeve Shirt - No Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_L.svg`, reference: null,
+        wearer: "Male", group: "Male Shirt", value: "long_sleeve", label: "Long Sleeve Shirt", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_L.svg`, reference: null,
     },
     {
-        wearer: "Male", group: "Male Shirt", value: "long_sleeve_with_tie", label: "Long Sleeve Shirt - With Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_L_Tie.svg`, reference: null,
+        wearer: "Male", group: "Male Shirt", value: "long_sleeve_tie", label: "Long Sleeve Shirt - With Tie", level: null, image: `/js/data/blueServiceUniform/images/Shirt_M_L_Tie.svg`, reference: null,
     },
 
     {
@@ -28,16 +28,28 @@ export default [
         wearer: "Female", group: "Female Slacks", value: "female_slacks_princess", label: "Female Slacks - Princess", level: null, image: `/js/data/blueServiceUniform/images/Slacks_P.svg`, reference: null,
     },
     {
-        wearer: "Female", group: "Female Shirt", value: "short_sleeve_no_tietab", label: "Short Sleeve Shirt - No Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_S.svg`, reference: null,
+        wearer: "Female", group: "Female Shirt", value: "short_sleeve", label: "Short Sleeve Shirt", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_S.svg`, reference: null,
     },
     {
-        wearer: "Female", group: "Female Shirt", value: "short_sleeve_with_tietab", label: "Short Sleeve Shirt - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_S_Tie.svg`, reference: null,
+        wearer: "Female", group: "Female Shirt", value: "short_sleeve_tietab", label: "Short Sleeve Shirt - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_S_Tie.svg`, reference: null,
     },
     {
-        wearer: "Female", group: "Female Shirt", value: "long_sleeve_no_tietab", label: "Long Sleeve Shirt - No Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_L.svg`, reference: null,
+        wearer: "Female", group: "Female Shirt", value: "long_sleeve", label: "Long Sleeve Shirt", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_L.svg`, reference: null,
     },
     {
-        wearer: "Female", group: "Female Shirt", value: "long_sleeve_with_tietab", label: "Long Sleeve Shirt - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_L_Tie.svg`, reference: null,
+        wearer: "Female", group: "Female Shirt", value: "long_sleeve_tietab", label: "Long Sleeve Shirt - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_L_Tie.svg`, reference: null,
+    },
+    {
+        wearer: "Female", group: "Female Shirt", value: "short_sleeve_princess", label: "Short Sleeve Shirt Princess", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_P_S.svg`, reference: null,
+    },
+    {
+        wearer: "Female", group: "Female Shirt", value: "short_sleeve_princess_tietab", label: "Short Sleeve Shirt Princess - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_P_S_Tie.svg`, reference: null,
+    },
+    {
+        wearer: "Female", group: "Female Shirt", value: "long_sleeve_princess", label: "Long Sleeve Shirt Princess", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_P_L.svg`, reference: null,
+    },
+    {
+        wearer: "Female", group: "Female Shirt", value: "long_sleeve_tietab", label: "Long Sleeve Shirt Princess - With Tie Tab", level: null, image: `/js/data/blueServiceUniform/images/Shirt_F_P_L_Tie.svg`, reference: null,
     },
     {
         wearer: "Female", group: "Female Skirt", value: "female_skirt", label: "Skirt", level: null, image: `/js/data/blueServiceUniform/images/Skirt.svg`, reference: null,
